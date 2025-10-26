878ff8fb0571f818e7ba89dae06d401ef6d0c34e
Afraz-22 <afrazur.r@gmail.com>
2025-10-26 22:58:06 +0600

    Add files via upload

---
+++ prac01.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+bool isNum() {
+
+    string c;
+
+    cin >> c;
+
+    for (int i = 0; i < c.length(); i++) {
+
+        if (c[i]>47 && c[i]<57) {
+
+           continue;}
+
+           else return false;
+
+        }
+
+    return true;
+
+}
+
+int main() {
+
+    cout << "Enter a value: ";
+
+    if (isNum()) {
+
+        cout << "The input is numeric." << endl;
+
+    } else {
+
+        cout << "The input is not numeric." << endl;
+
+    }
+
+    return 0;
+
+}
+
+
